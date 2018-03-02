e3c5b31d51f43bd4e1f400ba1dc567aad3b96e23
Matthew Mogavero <matt.moga@gmail.com>
2018-03-02 18:36:17 -0500

    created redux store, a count reducer, and implemented inc and dec actions, but the React app isn't rerendering

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import { createStore } from 'redux';
+
+// initial state
+const initialState = 0;
+// reducer (how state changes)
+const countReducer = (state=initialState, action) => {
+  switch(action.type) {
+    case "INCREMENT": 
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+    default:
+      return state
+  }
+};
+// creation of the store
+const store = createStore(
+  countReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+
 class App extends Component {
   render() {
     return (
       <div>
-        Hello
+        <h1>{store.getState()}</h1>
+        <button onClick={() => store.dispatch({type: 'INCREMENT'})}>+</button>
+        <button onClick={() => store.dispatch({type: 'DECREMENT'})}>-</button>
       </div>
     );
   }
