6b38d726978976bc4d3e67c6203e1fc69635fecc
Matthew Mogavero <matt.moga@gmail.com>
2018-03-02 19:48:25 -0500

    created mapDispatchToProps to provide increment and decrement to the app

--- src/App.js
+++ src/App.js
@@ -4,20 +4,27 @@ import { connect } from 'react-redux';
 
 class App extends Component {
   render() {
-    console.log(this);
     return (
       <div>
         <h1>{this.props.count}</h1>
-        <button onClick={() => this.props.dispatch({type: 'INCREMENT'})}>+</button>
-        <button onClick={() => this.props.dispatch({type: 'DECREMENT'})}>-</button>
+        <button onClick={this.props.increment}>+</button>
+        <button onClick={this.props.decrement}>-</button>
       </div>
     );
   }
 }
+
 const mapStateToProps = (state) => {
   return {
     count: state
   }
 }
 
-export default connect(mapStateToProps, null)(App);
+const mapDispatchToProps = (dispatch) => {
+  return {
+    increment: () => dispatch({type: 'INCREMENT'}),
+    decrement: () => dispatch({type: 'DECREMENT'}),
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
